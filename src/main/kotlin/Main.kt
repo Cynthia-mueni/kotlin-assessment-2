aada62699e9b506ec59ac5953f5467ba8cb8e31a
Cynthia-mueni <nthenyamuenicynthia@gmail.com>
2024-03-25 08:23:24 +0300

    added file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main() {
+    numbers(40,20)
+    Boolean
+}
+//1
+fun word(word:String){
+    var sentence="Barnie bakes brown bagels and buns"
+    println(sentence)
+}
+
+//2
+fun numbers(num1:Int,num2:Int):Int{
+    var sum= num1+num2
+    var average=num1+num2/2
+    var count=num1+num2
+    return sum
+    return average
+    return count
+}
+//3
+fun numbers (num:Int){
+    var radius=20
+    var pie= 3.14159
+    var volume =4/3 * 3.14159 * 20
+    println(volume)
+}
+//4
+fun isPalindrome(word:String):Boolean{
+
+    return isPalindrome("pop")
+
+}
+
